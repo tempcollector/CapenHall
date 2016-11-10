d38f2ab52899457ac5e0be050ff0b900d4270b7c
Richard Zhunio <richard-284@hotmail.com>
2016-11-10 08:14:35 -0500

    Update

--- code/reader.py
+++ code/reader.py
@@ -21,31 +21,42 @@ def getMACAddr(interface):
         macAddress = read(filename)
     except:
         macAddress = read(MAC_ADDRESS_eth0)
-    return macAddress
+    return macAddress.strip()
 def readTemperature(filename):
     """Reads the temperature in degrees celcius from a file.
     It returns the recorded time of the temperature along with the
-    temperature in degrees celcius and farenheit."""
+    temperature in degrees celcius and farenheit.
+    A TempSensorError can be thrown in two cases:
+    1) if the temperature sensor is disconnected and the file that contains
+    the temperature does not exist.
+    2) If the file exists but the temperature sensor was quickly disconnected
+    after the file was read."""
     print 'Reading temperature...'
     try :
+        # The temperature sensor can be disconnected and thus the file
+        # containing the temperature would not exist
         data = read(filename)
     except IOError:
-        raise TempSensorError('The temperature sensor is discconected.')
-    currentTime = time.strftime('%x %X %Z')
-    celcius = __parseTemperature(data)
-    farenheit = round(celcius * 9.0 / 5.0 + 32.0, 3)
-    return (currentTime, celcius, farenheit)
+        raise TempSensorError('The temperature sensor is disconected.')
+    else:
+        currentTime = time.strftime('%x %X %Z')
+        # A TempSensorError can be thrown if the file containig the temperature
+        # does not have valid data
+        celcius = __parseTemperature(data)
+        farenheit = round(celcius * 9.0 / 5.0 + 32.0, 3)
+        return (currentTime, celcius, farenheit)
 def __parseTemperature(data):
     """Parses the temperature in degreees celcius from a string.
     The string contains the following informattion in two lines:
     1) the cycle redundancy check (CRC)
     2) the temperature in celcius.
-    If the CRC is 00, then the temperature sensor is discconected;
-    otherwise, the temperature readings should be fine."""
+    If the CRC is 00, then the temperature sensor is discconected and a
+    TempSensorError is thrown; otherwise, the temperature readings should
+    be fine."""
     data = data.split('\n')
-    checksum = data[0].find('crs=00')
+    checksum = data[0].find('crc=00')
     celcius = data[1].split('t=')
-    if checksum == 'crc=00':
+    if checksum != -1:
         raise TempSensorError('The temperature sensor is discconected.')
     return float(celcius[1]) / 1000
 class TempSensorError(Exception):
--- code/tester.py
+++ code/tester.py
@@ -1,41 +1,45 @@
 #!/usr/bin/python
 import reader
+import database
+import location
 import credentials as c
+from reader import TempSensorError
 
 from apiclient.discovery import build
 from oauth2client.service_account import ServiceAccountCredentials
 from httplib2 import Http
 
 def main():
-    databaseFile = '../files/temperature.db'
-    filename = '/sys/bus/w1/devices/28-00044a3b10ff/w1_slave'
-    interface = 'eth0'
-    place = 'Kitchen'
+    databaseFile = 'temperature.db'
+    tempFile = '/sys/bus/w1/devices/28-00044a3b10ff/w1_slave'
+    interface = 'wlan0'
 
-    macAddr = reader.getMACAddr(interface)
-    (time, celcius, farenheit) = reader.readTemperature(filename)
-
-    spreadsheet = {
-        'ID' : '1PY-eVqQs-2LdJu3gZ2tdaBRBNFh0gpI1GdSWpL_yXd8',
-        'rangeName' : 'A1:B1',
-        'valueInputOption' : 'RAW',
-        'ROWS' : [time, celcius, farenheit, place]
-    }
-
-    #location = {macAddr: place}
-    database.uploadToDB(databaseFile, (t, c, f), location[hwAddr])
-    credentials = c.get_credentials()
-    http_auth = credentials.authorize(Http())
-    service = build('sheets', 'v4', http = http_auth)
-
-    values = [spreadsheet['ROWS']]
-    body = {'values': values}
-    result = service.spreadsheets().values().append(
-        spreadsheetId = spreadsheet['ID'],
-        range = spreadsheet['rangeName'],
-        valueInputOption = spreadsheet['valueInputOption'],
-        body = body).execute()
-    print 'Uploading to Google Sheets...'
+    try:
+        place = location.PI[reader.getMACAddr(interface)]
+        (time, celcius, farenheit) = reader.readTemperature(tempFile)
+    except KeyError as error:
+        print error.message, ': device not registered.'
+    except TempSensorError as error:
+        print place, ': ', error.message
+    else:
+        spreadsheet = {
+            'ID' : '1PY-eVqQs-2LdJu3gZ2tdaBRBNFh0gpI1GdSWpL_yXd8',
+            'rangeName' : 'A1:B1',
+            'valueInputOption' : 'RAW',
+            'ROWS' : [time, celcius, farenheit, place]
+        }
+        database.uploadToDB(databaseFile, (time, celcius, farenheit), place)
+        credentials = c.get_credentials()
+        http_auth = credentials.authorize(Http())
+        service = build('sheets', 'v4', http = http_auth)
+        values = [spreadsheet['ROWS']]
+        body = {'values': values}
+        print 'Uploading to Google Sheets...'
+        result = service.spreadsheets().values().append(
+            spreadsheetId = spreadsheet['ID'],
+            range = spreadsheet['rangeName'],
+            valueInputOption = spreadsheet['valueInputOption'],
+            body = body).execute()
 
 if __name__ == '__main__':
     main()
