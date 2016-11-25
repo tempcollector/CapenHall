c946567177875dd3437a40c6d4dc3136863ff764
tempCollector Newpaltz <tempcollector.newpaltz.gmail.com>
2016-11-25 13:36:39 -0500

    Modified PI.py, added Sheet IDs

--- code/PI.py
+++ code/PI.py
@@ -18,3 +18,22 @@ location = {
     '00:e0:4c:1d:48:56': 'F3SE',
     '00:e0:4c:1d:47:d8': 'F3SW'
 }
+Sheets={
+'F0NE': '1HcaB23dLH7cQjpeGu2rmHQuNIHkLWjxEK3Fp_aZnJDM',
+'F0NW': '1GfI6tscc3n494p-cQc-4qTraZy16rrHxfdb_MIh3eYQ',
+'F0SE': '1hDDvKWfvBvMFdp4SqGb-oLrbR0vgm_AOhUMjCoSFwAY',
+'F0SW': '1yaQFxf0sgbngdVfEPqhqRqQoiSe8Ti9z1LkXJrMkjYE',
+'F1NE': '14mdKwg_B9AI3o6jAllNmfv5jnlQte0JNL6mZ_oYel1w',
+'F1NW': '1ZO6vEekjwenBkeZkfpLPj5tNCQlpQZjTImheXeVNy_U',
+'F1SE': '16f7HsfxoTZEo1YLNY--IcazJzq6a3qqvFwD67N6Cku8',
+'F1SW': '1UyI5s-9tpN5wE6NjPyXpwNbCe9GPz_2S4bUkiJ1vWKY',
+'F2NE': '1Zscm5yPsaa6ga3EQ-UsmHwWSYpA_FsYDJhnSbpo0w-c',
+'F2NW': '1KAj7FKnJb9RnfTOWDmo-lArWRESDbVD_LIq28g65i4I',
+'F2SE': '1NwNtEXqYfnzr3siqjBdr0Qjli2c9tQcCO0pLJPmiflE',
+'F2SW': '1u9Crphc1pdiRf7gid_mohtsvps-J1hqussP1xl9dqkQ',
+'F3NE': '1XRsJLl3Wn9BcIvQpt2rPKjvJIWSz7DZRMMT0Gx93Jco',
+'F3NW': '1TpPw0RfvVkN220KOo_MO4om5ErCQUjckQRrFMXDoJxc',
+'F3SE': '1PnlM3zkUD830VaCYiVJ1Zzt_MOzAe-V7_EX2jDV_Zo0',
+'F3SW': '1kzn9e3gjaxmMwPP_5xXe8pTVVfo-YcqCG8aeM5NRkrc'
+}
+
